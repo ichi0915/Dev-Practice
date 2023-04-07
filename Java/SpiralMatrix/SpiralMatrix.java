ff72956fa75d95af501a272b12c53264a0edaba7
ichi0915 <ichi0915@gmail.com>
2023-04-07 11:41:13 -0600

    fix(gitignore): Update to only have one .gitignore file

--- Java/SpiralMatrix/SpiralMatrix.java
+++ Java/SpiralMatrix/SpiralMatrix.java
@@ -31,9 +31,85 @@ class SpiralMatrix {
 
 	public static void main(final String[] args) {
 		int[][] matrix = {{1,2,3},{4,5,6},{7,8,9}};
+		// int[][] matrix = {{1,2,3,4},{5,6,7,8},{9,10,11,12}};
 		List<Integer> finalMatrix = spiralOrder(matrix);
 
+		System.out.println("Result matrix: " + finalMatrix);
 		// matrix = {{2,3,7,1},{9,8,6,7},{1,7,6,4,8}};
+	}
+
+	//directions 1:right 2:down 3:left 4:up
+	public static void spiralTraversal(List<Integer> list, int[][] matrix, int startWidth, int startHeight, int direction, int loopTimes) {
+		//Final list
+		// List<Integer> list = new ArrayList<>();
+		//Actual length
+		int matrixWidth = matrix.length;
+		int matrixHeight = matrix[0].length;
+		//My current position
+		int currentWidth = 0;
+		int currentHeight = 0;
+
+		System.out.println("\ndirection " + direction);
+
+		if (1 == direction || 3 == direction) {
+			startWidth += loopTimes;
+			startHeight += loopTimes;
+		}
+		else if (2 == direction || 4 == direction) {
+			startWidth -= loopTimes;
+			startHeight -= loopTimes;
+		}
+
+		System.out.print("startWidth:" + startWidth + " startHeight" + startHeight + " matrixWidth:"+ matrixWidth + " matrixHeight:"+ matrixHeight + "\n");
+
+		if (1 == direction) {
+			int m = startWidth;
+			for(int n = startHeight; n < matrixHeight && n >= 0; n++) {
+			// for(int n = startHeight; n < matrixHeight; n++) {
+				System.out.print(matrix[m][n] + " ");
+				list.add(matrix[m][n]);
+				currentWidth = m + 1;
+				currentHeight = n;
+			}
+		}
+		else if (2 == direction) {
+			int n = startHeight;
+			for(int m = startWidth; m < matrixWidth && m >= 0; m++) {
+				System.out.print(matrix[m][n] + " ");
+				list.add(matrix[m][n]);
+				currentWidth = m;
+				currentHeight = n - 1;
+			}
+		}
+		else if (3 == direction) {
+			int m = startWidth;
+			for(int n = startHeight; n < matrixHeight && n >= 0; n--) {
+				System.out.print(matrix[m][n] + " ");
+				list.add(matrix[m][n]);
+				currentWidth = m - 1;
+				currentHeight = n;
+			}
+		}
+		else if (4 == direction) {
+			int n = startHeight;
+			for(int m = startWidth; m >= matrixWidth-2 && m >= 0; m--) {
+				// System.out.print("M:" + m + " n" + n + " matrixWidth:"+ matrixWidth + " --");
+				System.out.print(matrix[m][n] + " ");
+				list.add(matrix[m][n]);
+				currentWidth = m;
+				currentHeight = n - 1;
+			}
+		}
+
+		direction++;
+		if (4 < direction){
+			direction = 1;
+			loopTimes++;
+		}
+
+		if ( (matrixWidth*matrixHeight) > list.size() ) {
+			spiralTraversal(list, matrix, currentWidth, currentHeight, direction, loopTimes);
+		}
 	}
 
 	public static List<Integer> spiralOrder(int[][] matrix) {
@@ -41,6 +117,16 @@ class SpiralMatrix {
 
 		System.out.println("Input matrix: " + Arrays.deepToString(matrix));
 
+		spiralTraversal(finalMatrix, matrix, 0, 0, 1, 0);
+
+		// for(int m = 0; m < matrixWidth; m++) {
+		// 	for(int n = 0; n < matrixHeight; n++) {
+		// 		System.out.print(matrix[m][n] + " ");
+		// 		list.add(matrix[m][n]);
+		// 	}
+		// }
+
 		return finalMatrix;
 	}
 }
+//30
