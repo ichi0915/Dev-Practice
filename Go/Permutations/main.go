ec1c0a34192a6aca9b9a83fb6c5844ad25a1ea35
ichi0915 <ichi0915@gmail.com>
2023-07-30 08:35:00 -0600

    feat(Permutations): Added only the question

---
+++ Go/Permutations/main.go
@@ -0,0 +1,43 @@
+/*
+Dev Practice - Permutations:
+
+	Given an array nums of distinct integers, return all the possible permutations.
+	You can return the answer in any order.
+
+	Example 1:
+	Input: nums = [1,2,3]
+	Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+
+	Example 2:
+	Input: nums = [0,1]
+	Output: [[0,1],[1,0]]
+
+	Example 3:
+	Input: nums = [1]
+	Output: [[1]]
+
+	Constraints:
+		1 <= nums.length <= 6
+		-10 <= nums[i] <= 10
+		All the integers of nums are unique.
+*/
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Response: ", permute([]int{1, 2, 3})) // response [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+	// fmt.Println("Response: ", permute([]int{0, 1}))    // response [[0,1],[1,0]]
+	// fmt.Println("Response: ", permute([]int{1})) // response [[1]]
+}
+
+func permute(nums []int) [][]int {
+	var response [][]int
+
+	//
+
+	return response
+}
