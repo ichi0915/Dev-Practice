22a4519d113ab01a9a32666b5d29ded6e82ed005
ichi0915 <ichi0915@gmail.com>
2024-04-29 12:11:09 -0600

    feat(mysql): Managers with at Least 5 Direct Reports

---
+++ mysql/13-ManagersWithAtLeast5DirectReports.sql
@@ -0,0 +1,56 @@
+-- 13 - Managers with at Least 5 Direct Reports
+
+-- Table: Employee
+--
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | id          | int     |
+-- | name        | varchar |
+-- | department  | varchar |
+-- | managerId   | int     |
+-- +-------------+---------+
+-- id is the primary key (column with unique values) for this table.
+-- Each row of this table indicates the name of an employee, their department, and the id of their manager.
+-- If managerId is null, then the employee does not have a manager.
+-- No employee will be the manager of themself.
+--
+-- Write a solution to find managers with at least five direct reports.
+--
+-- Return the result table in any order.
+--
+-- The result format is in the following example.
+--
+--
+-- Example 1:
+--
+-- Input:
+-- Employee table:
+-- +-----+-------+------------+-----------+
+-- | id  | name  | department | managerId |
+-- +-----+-------+------------+-----------+
+-- | 101 | John  | A          | null      |
+-- | 102 | Dan   | A          | 101       |
+-- | 103 | James | A          | 101       |
+-- | 104 | Amy   | A          | 101       |
+-- | 105 | Anne  | A          | 101       |
+-- | 106 | Ron   | B          | 101       |
+-- +-----+-------+------------+-----------+
+--
+-- Output:
+-- +------+
+-- | name |
+-- +------+
+-- | John |
+-- +------+
+
+
+-- ### answer ###
+
+SELECT Employee.name FROM Employee
+INNER JOIN Employee AS EmployeeManager ON Employee.id=EmployeeManager.managerId
+GROUP BY EmployeeManager.managerId
+HAVING COUNT(EmployeeManager.managerId) >= 5
+
+-- Notes to self:
+-- CROSS JOIN keyword returns all records from both tables
